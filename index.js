2eac332fe2be67c84d6b73113272882f0d0abd71
DiLaPe9 <dpetrov@bhb.bg>
2025-04-09 02:41:15 +0300

    latest fix

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 });
   useNewUrlParser: true,
   useUnifiedTopology: true,
