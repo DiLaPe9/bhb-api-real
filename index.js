9ceb4930a6fedc0627089ae60a34bfd3f98780ad
DiLaPe9 <dpetrov@bhb.bg>
2025-04-09 23:11:05 +0300

    fix models / product

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const { syncAsbisProducts } = require("./syncAsbis");
-const Product = require("./productModel");
+const Product = require("./models/productModel");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
